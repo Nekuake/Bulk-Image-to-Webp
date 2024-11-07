cf74b4c2d649a318c915505b334b52f8083361ba
Nekuake - Alejandro Hurtado <Nekuake@protonmail.com>
2024-11-07 12:26:14 +0100

    Resizing has been disabled to fit current needs

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from tkinter import filedialog
 import os
 
 
-def convertionImage(imagePath="", finalDir=""):
+def convertionImage(imagePath="", finalDir="", resize_images=False, quality=70):
     print(imagePath)
     image = Image.open(imagePath)
-    if image.width > image.height:
-        image = image.resize((512, int(image.height * 512 / image.width)))
-    elif image.width < image.height:
-        image = image.resize((int(image.width * 512 / image.height), 512))
-    else:
-        image = image.resize((512, 512))
-    image.save(finalDir + "/" + (os.path.basename(imagePath).rsplit(".")[0]) + ".webp", "webp")
+    if resize_images:
+        if image.width > image.height:
+            image = image.resize((512, int(image.height * 512 / image.width)))
+        elif image.width < image.height:
+            image = image.resize((int(image.width * 512 / image.height), 512))
+        else:
+            image = image.resize((512, 512))
+    image.save(finalDir + "/" + (os.path.basename(imagePath).rsplit(".")[0]) + ".webp", "webp", quality=quality)
 
 
 print(sys.argv)  # Might add drag-and-drop support
@@ -26,5 +27,6 @@ finalDir = os.path.join(containerDir, "WebP")
 if not os.path.isdir(finalDir):
     os.mkdir(finalDir)
 imagesList = next(os.walk(containerDir), (None, None, []))[2]  # [] if no file
+quality=int(input("Which quality? 0-100: ").strip())
 for x in range(len(imagesList)):
-    convertionImage(os.path.join(containerDir, imagesList[x]), finalDir)
+    convertionImage(os.path.join(containerDir, imagesList[x]), finalDir,  quality)
